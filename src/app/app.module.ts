7210e535250e072b4587a64cb40fc87aa3df9f1e
derrrick william <williamderrick100@gmail.com>
2019-01-11 10:42:51 +0300

    Added Quote form.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QuotesComponent } from './quotes/quotes.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent,
     QuoteComponent,
     QuoteDetailsComponent
   ],
