0f4d8377b33e03db590f1475182e5b6571f87a62
derrrick william <williamderrick100@gmail.com>
2019-01-14 17:11:33 +0300

    Removed form Validation.

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Quote } from  '../quote';
+import { Quote } from '../quote'
+import { DatePipe } from '../../../node_modules/@angular/common';
 
 @Component({
   selector: 'app-quote',
@@ -7,13 +8,24 @@ import { Quote } from  '../quote';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-  quotes= [];
+
+  quotes = [];
+
+  deleteQuote(isComplete, index) {
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete this Quote${this.quotes[index].quote}`)
+      if (toDelete) {
+        this.quotes.splice(index, 1);
+      }
+    }
+  }
 
   addNewQuote(quote) {
     let quoteLength = this.quotes.length;
     quote.id = quoteLength + 1;
-    quote.completeDate = new Date(quote.completeDate);
-    this.quotes.push(quote);
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
+
   }
   constructor() { }
 
