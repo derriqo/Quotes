5ef79b0dca461f8d598b5e73f3c4c3ee823f4cda
derrrick william <williamderrick100@gmail.com>
2019-01-14 11:12:56 +0300

    Added form Validation.

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
 import {Quote} from '../quote';
 
 @Component({
@@ -8,7 +8,12 @@ import {Quote} from '../quote';
 })
 export class QuoteFormComponent implements OnInit {
 newQuote=new Quote(0,"","","",new Date());
-  constructor() { }
+@Output() addQuote= new EventEmitter<Quote>();
+
+submitQuote(){
+     this.addQuote.emit(this.newQuote);
+}
+  constructor() {}
 
   ngOnInit() {
   }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-quote',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit() {
